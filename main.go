3c425747bd304faf0ad9327144abdf0c71e58111
mooyun <myan5689@163.com>
2024-02-09 22:17:32 +0800

    add server

--- main.go
+++ main.go
@@ -1,4 +1,16 @@
 package main
-func main(){
-	
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	server := http.Server{
+		Addr:"127.0.0.1:8080",
+	}
+
+	if err:= server.ListenAndServe(); err != nil {
+		log.Println(err)
+	}
 }
