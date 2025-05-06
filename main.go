13aa4ce27f1010dbfd10856ecdcdca69c77ebd17
mooyun <myan5689@163.com>
2025-05-06 09:57:40 +0800

    nothing

--- main.go
+++ main.go
@@ -5,36 +5,37 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"path"
+	_ "path"
 	"text/template"
 )
 
-type IndexDate struct{
+type IndexDate struct {
 	Title string `json:"title"`
-	Desc string `json:"desc"`
-
+	Desc  string `json:"desc"`
 }
-func index(w http.ResponseWriter,r *http.Request) {
-	w.Header().Set("Content-type","application/json")
-	var indexDate IndexDate
-	indexDate.Title="spgblg"
-	indexDate.Desc="刚刚开始"
 
-	t:=template.New("index.h0tml")
-	path,_:=os.Getwd()
-	t,_=t.ParseFiles(path+"/template/index.html")
-	t.Execute(w,indexDate)
-	
-	jsonStr,_:=json.Marshal(indexDate)
+func index(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/json")
+	var indexDate IndexDate
+	indexDate.Title = "spgblg"
+	indexDate.Desc = "刚刚开始"
+
+	t := template.New("index.html")
+	path, _ := os.Getwd()
+	t, _ = t.ParseFiles(path + "/template/index.html")
+	t.Execute(w, indexDate)
+
+	jsonStr, _ := json.Marshal(indexDate)
 	w.Write([]byte(jsonStr))
 }
+
 func main() {
 	server := http.Server{
-		Addr:"127.0.0.1:8080",
+		Addr: "127.0.0.1:8080",
 	}
 
-	http.HandleFunc("/",index)
-	if err:= server.ListenAndServe(); err != nil {
+	http.HandleFunc("/", index)
+	if err := server.ListenAndServe(); err != nil {
 		log.Println(err)
 	}
 }
